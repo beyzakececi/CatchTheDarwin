2c7e2d4852ef6781cf0089ecb6c67cb77386a4b5
beyzakececi <beyzakececi.35@gmail.com>
2023-07-19 19:46:12 +0300

    Add increase score

--- app/src/main/java/com/beyzakececi/catchthedarwin/MainActivity.kt
+++ app/src/main/java/com/beyzakececi/catchthedarwin/MainActivity.kt
@@ -2,15 +2,22 @@ package com.beyzakececi.catchthedarwin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.beyzakececi.catchthedarwin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var score=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         val view=binding.root
         setContentView(view)
+    }
+
+    fun increaseScore(view: View){
+        score+=1
+        binding.scoreText.text="Score: ${score}"
     }
 }
